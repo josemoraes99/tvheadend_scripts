64c0d5ec8ec9f2b75fc5b0b3c6d25e247475846d
Jose Moraes <mail.jmoraes@gmail.com>
2020-06-17 18:53:44 -0300

    [update_17_06_2020]

--- category-filter.pl
+++ category-filter.pl
@@ -4,84 +4,84 @@
 # The categories recognized by tvheadend (see epg.c) 
 #  
 
-my $MOVIE             =    "Movie / Drama";
-my $THRILLER          =    "Detective / Thriller";
-my $ADVENTURE         =    "Adventure / Western / War";
-my $SF                =    "Science fiction / Fantasy / Horror";
+my $MOVIE             =    "Drama";
+my $THRILLER          =    "Thriller";
+my $ADVENTURE         =    "Adventure";
+my $SF                =    "Science fiction";
 my $COMEDY            =    "Comedy";
-my $SOAP              =    "Soap / Melodrama / Folkloric";
+my $SOAP              =    "Soap";
 my $ROMANCE           =    "Romance";
-my $HISTORICAL        =    "Serious / Classical / Religious / Historical movie / Drama";
-my $XXX               =    "Adult movie / Drama";
+my $HISTORICAL        =    "History";
+my $XXX               =    "Adult movie";
 
-my $NEWS              =    "News / Current affairs";
-my $WEATHER           =    "News / Weather report";
+my $NEWS              =    "News";
+my $WEATHER           =    "Weather";
 my $NEWS_MAGAZINE     =    "News magazine";
 my $DOCUMENTARY       =    "Documentary";
-my $DEBATE            =    "Discussion / Interview / Debate";
+my $DEBATE            =    "Interview";
 my $INTERVIEW         =    $DEBATE ;
 
-my $SHOW              =    "Show / Game show";
-my $GAME              =    "Game show / Quiz / Contest";
-my $VARIETY           =    "Variety show";
+my $SHOW              =    "Show";
+my $GAME              =    "Game show";
+my $VARIETY           =    "Variety";
 my $TALKSHOW          =    "Talk show";
 
 my $SPORT             =    "Sports";
-my $SPORT_SPECIAL     =    "Special events (Olympic Games; World Cup; etc.)";
+my $SPORT_SPECIAL     =    "Sports";
 my $SPORT_MAGAZINE    =    "Sports magazines";
-my $FOOTBALL          =    "Football / Soccer";
-my $TENNIS            =    "Tennis / Squash";
+my $FOOTBALL          =    "Football";
+my $TENNIS            =    "Tennis";
 my $SPORT_TEAM        =    "Team sports (excluding football)";
 my $ATHLETICS         =    "Athletics";
 my $SPORT_MOTOR       =    "Motor sport";
 my $SPORT_WATER       =    "Water sport";
 
-my $KIDS              =    "Children's / Youth programmes";
-my $KIDS_0_5          =    "Pre-school children's programmes";
-my $KIDS_6_14         =    "Entertainment programmes for 6 to 14";
-my $KIDS_10_16        =    "Entertainment programmes for 10 to 16";
-my $EDUCATIONAL       =    "Informational / Educational / School programmes";
-my $CARTOON           =    "Cartoons / Puppets";
+my $KIDS              =    "Children";
+my $KIDS_0_5          =    "Children";
+my $KIDS_6_14         =    "Children";
+my $KIDS_10_16        =    "Children";
+my $EDUCATIONAL       =    "Educational";
+my $CARTOON           =    "Cartoon";
 
-my $MUSIC             =    "Music / Ballet / Dance";
+my $MUSIC             =    "Music";
 my $ROCK_POP          =    "Rock / Pop";
-my $CLASSICAL         =    "Serious music / Classical music";
-my $FOLK              =    "Folk / Traditional music";
+my $CLASSICAL         =    "Classical";
+my $FOLK              =    "Folk";
 my $JAZZ              =    "Jazz";
-my $OPERA             =    "Musical / Opera";
+my $OPERA             =    "Musical";
 
-my $CULTURE           =    "Arts / Culture (without music)";
+my $CULTURE           =    "Culture";
 my $PERFORMING        =    "Performing arts";
 my $FINE_ARTS         =    "Fine arts";
 my $RELIGION          =    "Religion";
-my $POPULAR_ART       =    "Popular culture / Traditional arts";
+my $POPULAR_ART       =    "Arts";
 my $LITERATURE        =    "Literature";
-my $FILM              =    "Film / Cinema";
+my $FILM              =    "Cinema";
 my $EXPERIMENTAL_FILM =    "Experimental film / Video";
-my $BROADCASTING      =    "Broadcasting / Press";
+my $BROADCASTING      =    "Press";
 
-my $SOCIAL            =    "Social / Political issues / Economics";
-my $MAGAZINE          =    "Magazines / Reports / Documentary";
-my $ECONOMIC          =    "Economics / Social advisory";
+my $SOCIAL            =    "Political";
+my $MAGAZINE          =    "Magazine";
+my $ECONOMIC          =    "Economics";
 my $VIP               =    "Remarkable people";
 
-my $SCIENCE           =    "Education / Science / Factual topics";
-my $NATURE            =    "Nature / Animals / Environment";
-my $TECHNOLOGY        =    "Technology / Natural sciences";
+my $SCIENCE           =    "Science";
+my $NATURE            =    "Nature";
+my $TECHNOLOGY        =    "Science";
 my $DIOLOGY           =    $TECHNOLOGY;
-my $MEDECINE          =    "Medicine / Physiology / Psychology";
-my $FOREIGN           =    "Foreign countries / Expeditions";
-my $SPIRITUAL         =    "Social / Spiritual sciences";
+my $MEDECINE          =    "Medical";
+my $FOREIGN           =    "Documentary";
+my $SPIRITUAL         =    "Religious";
 my $FURTHER_EDUCATION =    "Further education";
 my $LANGUAGES         =    "Languages";
 
 my $HOBBIES           =    "Leisure hobbies";
-my $TRAVEL            =    "Tourism / Travel";
+my $TRAVEL            =    "Travel";
 my $HANDICRAF         =    "Handicraft";
 my $MOTORING          =    "Motoring";
-my $FITNESS           =    "Fitness and health";
+my $FITNESS           =    "Fitness health";
 my $COOKING           =    "Cooking";
-my $SHOPPING          =    "Advertisement / Shopping";
+my $SHOPPING          =    "Shopping";
 my $GARDENING         =    "Gardening";
 
 #
@@ -91,132 +91,137 @@ my $GARDENING         =    "Gardening";
 #
 
 my %REPLACE=(
-    "Ação"               => $ADVENTURE ,
-    "Drama"              => $MOVIE ,
-    "Comédia"            => $COMEDY ,
-    "Ficção"             => $SF ,
-    "Policial"           => $THRILLER ,
-    "Romance"            => $ROMANCE ,
-    "Suspense"           => $THRILLER ,
-    "Terror"             => $THRILLER ,
-    "Aventura"           => $ADVENTURE ,
-    "Pesca"              => $NATURE,
-    "Musical"            => $MUSIC ,
-    "Documentário"       => $DOCUMENTARY ,
-    "Débat"              => $SOCIAL ,
-    "Animação"           => $CARTOON ,
-    "Educativo"          => $EDUCATIONAL ,
-    "Show"               => $MUSIC ,
-    "Viagem"             => $TRAVEL ,
-    "Erótico"            => $XXX ,
-    "Meio Ambiente"      => $NATURE ,
-    "Cultural"           => $CULTURE ,
-    "Meteorologia"       => $WEATHER ,
-    "Saúde"              => $MEDECINE ,
-    "Ciência"            => $SCIENCE ,
-    "Culinária"          => $COOKING ,
-    "Comportamento"      => $VARIETY ,
-    "Debate"             => $DEBATE ,
-    "Game Show"          => $GAME ,
-    "Político"           => $NEWS ,
-    "Cinema"             => $MOVIE ,
-    "Moda e Estilo"      => $VARIETY ,
-    "Ecologia"           => $NATURE ,
-    "Western"            => $ADVENTURE ,
-    "Sobrenatural"       => $SPIRITUAL ,
-    "Biografia"          => $DOCUMENTARY ,
-    "Histórico"          => $HISTORICAL ,
-    "A Terra está em perigo"      => $DOCUMENTARY ,
-    "Televendas"         => $SHOPPING ,
-    "Investigação"       => $DOCUMENTARY ,
-    "Talk Show"          => $TALKSHOW ,
-    "Épico"              => $DOCUMENTARY ,
-    "Juvenil"            => $KIDS_10_16 ,
-    "Programa"           => $VARIETY ,
-    "Teatro"             => $PERFORMING ,
-    "Histórica"          => $HISTORICAL ,
-    "Guerra"             => $ADVENTURE ,
-    "Atletismo"          => $ATHLETICS ,
-    "Automobilismo"      => $SPORT_MOTOR ,
-    "Desenho"            => $CARTOON ,
-    "Surfe"              => $SPORT_WATER ,
-    "Radical"            => $SPORT ,
-    "Dança"              => $PERFORMING ,
-    "Artes"              => $PERFORMING ,
-    "Balé"               => $PERFORMING ,
-    "Ópera"              => $OPERA ,
-    "Competição"         => $GAME ,
-    "Medicina"           => $MEDECINE ,
-    "Comunicação"        => $BROADCASTING ,
-    "Turismo"         => $TRAVEL ,
-    
-    "Animação De Jennifer Coyle"                                                      => 0 ,
-    "Musical De Bernardo Mendonça"                                                      => 0 ,
-    "Mulher, socialista convicta, entra em coma e desperta 8 meses depois"                                                      => 0 ,
-    "Alex é um engenheiro cibernético"                                                      => 0 ,
-    "Musical De Gabriela Figueiredo"                                                      => 0 ,
-    
-    
-    "A França está sob ocupação"                                                      => 0 ,
-    "Comédia De Tim Heidecker"                                                      =>$COMEDY ,
-    "A séries conta a história de duas famílias, os Canon e os Montoya, donos de grandes áreas do Arizona"            => 0 ,
-    "Drew é um garoto tímido da cidade e Sherrie uma garota do interior"            => 0 ,
-    "O irmão de Rich quer uma Smith &amp; Wesson 686 Plus personalizada"            => 0 ,
-    
-    "Divertissement"     => $VARIETY ,
-    "Novela"             => $SOAP ,
-    "Émission"           => 0,
-    "Feuilleton"         => $SOAP ,
-    "Fin"                => 0,
-    "Fin des programmes" => 0 ,
-    "Interview"          => $INTERVIEW ,
-    "Jeu"                => $GAME ,
-    "Infantil"           => $KIDS ,
-    "Informativo"        => $NEWS ,
-    "Loterie"            => 0 ,
-    "Magazine"           => $MAGAZINE ,
-    "Opéra"              => $OPERA ,
-    "Série"              => $MOVIE  ,
-    "Spectacle"          => $PERFORMING ,
-    
-    "Esportivo"          => $SPORT ,
+
+    "Fitness"           => $FITNESS ,
+	"Esportivo"          => $SPORT ,
     "Futebol"            => $FOOTBALL ,
+	"Futebol de Salão"   => $FOOTBALL ,
     "Basquete"           => $SPORT ,
+	"Vôlei"              => $SPORT ,
     "Luta"               => $SPORT ,
     "Futebol Feminino"   => $FOOTBALL ,
-    
-    "Entrevista"          => $TALKSHOW ,
-    "Téléfilm"           => $MOVIE ,
-    "Télé-réalité"       => $VARIETY ,
-    "Diversos"         => $VARIETY ,
+	"Beisebol"           => $SPORT ,
+	"Atletismo"          => $SPORT ,
+	"Tênis"              => $SPORT ,
+	"Boxe"               => $SPORT ,
+	"Rugby"              => $SPORT ,
+	"Automobilismo"      => $SPORT_MOTOR ,
+	"Debate / Esportivo" => $SPORT ,
+	"Surfe"              => $SPORT ,
+    "Radical"            => $SPORT ,
+	"Sports"             => $SPORT ,
+    "Ciclismo"           => $SPORT ,
     "Tiercé"             => $SPORT ,
+	"Jeu"                => $GAME ,	
+	"Erótico"            => $XXX ,
+	"Político"           => $NEWS ,
+	"Política"           => $NEWS ,
+    "Informativo"        => $NEWS ,
+	"Jornalismo"         => $NEWS ,
+    "Jornalismo/Informativo" => $NEWS ,
+	"Variedades/Entrevista" => $DEBATE ,
+	"Variedades/Debate"  => $DEBATE ,
+	"Débat"              => $DEBATE ,
+	"Debate"             => $DEBATE ,
+	"Entrevista"         => $TALKSHOW ,
+	"Talk Show"          => $TALKSHOW ,
+	"Interview"          => $INTERVIEW ,
+	"Meteorologia"       => $WEATHER ,
+	"Emission politique" => $SOCIAL,
+    "Politique"          => $SOCIAL ,
+	"Comunicação"        => $BROADCASTING ,
+    "Variedades/Religião"=> $RELIGION ,
+	"Séries/Religião"    => $RELIGION ,
+	"Religião"           => $RELIGION ,
+	"Medicina"           => $MEDECINE ,
+    "Saúde"              => $MEDECINE ,
+	"Sobrenatural"       => $SPIRITUAL ,
+	"Animação"           => $CARTOON ,
+	"Desenho"            => $CARTOON ,
+	"Anime"              => $CARTOON ,
+	"Infantil"           => $KIDS ,
+	"Juvenil"            => $KIDS ,
+	"Educativo"          => $EDUCATIONAL ,
+	"Documentário"       => $DOCUMENTARY ,
+	"Investigação"       => $DOCUMENTARY ,
+	"Biografia"          => $DOCUMENTARY ,
+	"Séries/Documentário" => $DOCUMENTARY ,
+	"Épico"              => $DOCUMENTARY ,
+	"Histórico"          => $HISTORICAL ,
+	"Histórica"          => $HISTORICAL ,
+	"Pesca"              => $NATURE,
+	"Meio Ambiente"      => $NATURE ,
+	"Ecologia"           => $NATURE ,
+	"Cultural"           => $CULTURE ,
+	"Variedades/Cultural" => $CULTURE ,
+	"Séries/Cultural"    => $CULTURE ,
+	"Musical"            => $MUSIC ,
+	"Show"               => $MUSIC ,
+	"Clip"               => $MUSIC ,
+	"Variedades/Musical" => $MUSIC ,
+	"Musique"            => $MUSIC ,
+	"Dança"              => $PERFORMING ,
+    "Artes"              => $PERFORMING ,
+    "Balé"               => $PERFORMING ,
+    "Teatro"             => $PERFORMING ,
+	"Variedades/Artes"   => $PERFORMING ,
+	"Spectacle"          => $PERFORMING ,
+	"Ópera"              => $MUSIC ,
+	"Comportamento"      => $VARIETY ,
+	"Moda"               => $VARIETY ,
+	"Moda e Estilo"      => $VARIETY ,
+	"Programa"           => $VARIETY ,
+	"Variedades/Diversos" => $VARIETY ,
+	"Diversos"           => $VARIETY ,
+	"Divertissement"     => $VARIETY ,
+	"Télé-réalité"       => $VARIETY ,    
     "Divers"             => $VARIETY ,
-    "Reality Show"             => $VARIETY ,
-    "Emission politique"             => $SOCIAL,
-    "Politique"             => $SOCIAL ,    
-    "Divers"             => $VARIETY ,            
-    "Religião"           => $RELIGION ,
-    "Musique"            => $MUSIC ,
-    "Fitness"           => $FITNESS ,
-    "Sports"           => $SPORT ,
-    "Ciclismo"         => $SPORT ,
-    "Clip"           => $MUSIC ,
-    "Anime"           => $CARTOON ,
-    "Humor"           => $COMEDY ,
-
- ) ; 
+    "Reality Show"       => $VARIETY ,        
+    "Divers"             => $VARIETY ,
+	"Série"              => $VARIETY  , 
+	"Culinária"          => $COOKING ,
+	"Magazine"           => $MAGAZINE ,
+	"Revista Feminina"   => $MAGAZINE ,
+	"Especial"           => $MAGAZINE,
+	"Novela"             => $SOAP ,
+	"Feuilleton"         => $SOAP ,
+	"Viagem"             => $TRAVEL ,
+	"Turismo"         	 => $TRAVEL ,
+	"Televendas"         => $SHOPPING ,
+	"Competição"         => $GAME ,
+	"Game Show"          => $GAME ,
+	"Suspense"           => $THRILLER ,
+    "Terror"             => $THRILLER ,
+	"Policial"           => $THRILLER ,
+	"Ação"               => $ADVENTURE ,
+	"Aventura"           => $ADVENTURE ,
+	"Western"            => $ADVENTURE ,
+	"Guerra"             => $ADVENTURE ,
+	"Drama"              => $MOVIE ,
+    "Cinema"             => $MOVIE ,
+	"Téléfilm"           => $MOVIE ,
+	"Romance"            => $ROMANCE ,
+	"Humor"              => $COMEDY ,
+	"Comédia"            => $COMEDY ,
+    "Ficção"             => $SF ,
+    "Ciência"            => $SCIENCE ,
+	"Émission"           => 0,    
+    "Fin"                => 0,
+    "Fin des programmes" => 0 ,	
+	"Loterie"            => 0 ,
+) ; 
 
 my $PRE  = '<category lang=\"pt\">' ;
 my $POST = '</category>'  ;
 
 sub myfilter {
-  my ($a) = @_;
-  if ( exists $REPLACE{$a} ) {     
-      return $REPLACE{$a} ;
-  } else {
-      print STDERR "Warning: Unmanaged category: '$a'\n" ;
-      return $a ;
-  }
+	my ($a) = @_;
+	if ( exists $REPLACE{$a} ) {     
+		return $REPLACE{$a} ;
+	} else {
+		print STDERR "Warning: Unmanaged category: '$a'\n" ;
+		return $a ;
+	}
 }
 
 while (<>) {
@@ -224,3 +229,4 @@ while (<>) {
     $line =~ s/($PRE)(.*)($POST)/"$1".myfilter("$2")."$3"/ge ;
     print $line;
 }
+    
